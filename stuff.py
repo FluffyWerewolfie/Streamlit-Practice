9ce4b074501137dd1c735e8738c034673b104b87
FluffyWerewolfie <160604994+FluffyWerewolfie@users.noreply.github.com>
2024-02-28 03:29:53 +0000

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -11,11 +11,14 @@ from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 df = pd.read_csv("data/Student_Marks2.csv")
 print(df.head())
-model.fit(df[['time_study','number_courses']],df[['Marks']])
-y_preds = model.predict(df[['time_study','number_courses']])
 
-m_score=model.score(df[['time_study','number_courses']],df[['Marks']])
+
+
+m_score=model.score(df[['time_study'],df[['Marks']])
+                        
 print(m_score)
+
+                    
 tab1, tab2, tab3,tab4 = st.tabs(["Study To Marks", "Course Count To Marks","Prediction","Raw Data"])
 
 tab1.subheader("Is there a relationship between studying time and marks got?")
@@ -24,8 +27,17 @@ tab1.scatter_chart(df,x='time_study',y='Marks')
 tab2.subheader("Is there a relationship between courses attended and marks got?")
 tab2.scatter_chart(df,x='number_courses',y='Marks')
 
+
+
 tab3.subheader("Is there a relationship between courses attended and marks got?")
-tab3.scatter_chart(y_preds)
+model.fit(df[['time_study'],df[['Marks']])
+y_preds = model.predict(df[['time_study'])
+tab3.line_chart(y_preds)
+
+model.fit(df[['number_courses'],df[['Marks']])
+y_preds = model.predict(df[['number_courses'])
+tab3.line_chart(y_preds)
+                        
 
 tab4.subheader("Numbers are superior")
 tab4.write(df)
