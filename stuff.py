6f63e6179db9721d01b8de757e939965b5a4185f
FluffyWerewolfie <160604994+FluffyWerewolfie@users.noreply.github.com>
2024-02-28 02:52:05 +0000

    Update stuff.py
    Added a new tab for raw data

--- stuff.py
+++ stuff.py
@@ -17,7 +17,7 @@ y_preds = model.predict(df[['time_study','number_courses']])
 m_score=model.score(df[['time_study','number_courses']],df[['Marks']])
 print(m_score)
 
-tab1, tab2, tab3 = st.tabs(["Study To Marks", "Course Count To Marks","Prediction"])
+tab1, tab2, tab3,tab4 = st.tabs(["Study To Marks", "Course Count To Marks","Prediction","Raw Data"])
 
 tab1.subheader("Is there a relationship between studying time and marks got?")
 tab1.scatter_chart(df,x='time_study',y='Marks')
@@ -28,3 +28,5 @@ tab2.scatter_chart(df,x='number_courses',y='Marks')
 tab3.subheader("Is there a relationship between courses attended and marks got?")
 tab3.scatter_chart(y_preds)
 
+tab4.subheader("Numbers are superior")
+tab4.write(df)
