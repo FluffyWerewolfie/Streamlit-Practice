13edb998400e8ccef6ba915d65d8a2c547bdbcac
FluffyWerewolfie <160604994+FluffyWerewolfie@users.noreply.github.com>
2024-03-02 23:05:27 +0000

    added neural network feature
    a NN using tensorflow trained on a small number dataset, overfitting into oblivion

--- stuff.py
+++ stuff.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import PolynomialFeatures
+import tensorflow as tf
+from tensorflow import keras
 
+from sklearn.preprocessing import PolynomialFeatures
+TF_ENABLE_ONEDNN_OPTS=0
 st.write("""
 # My first app
 Sure is nice to *try* new things out!
@@ -10,9 +13,9 @@ Sure is nice to *try* new things out!
 from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 df = pd.read_csv("data/Student_Marks2.csv")
-print(df.head())
+##print(df.head())
 
-main1,main2 = st.tabs(["Student Marks", "Extra"])
+main1,main2,main3 = st.tabs(["Student Marks", "Web App Y1 Statistics","TensorFlow Shenanigans"])
                     
 tab1, tab2, tab3,tab4 = main1.tabs(["Study To Marks", "Course Count To Marks","Prediction","Raw Data"])
 
@@ -41,3 +44,51 @@ tab4.write(df)
 statFile = pd.read_csv("data/tbl_statistics.csv")
 webTraffic = statFile.drop(['statisticID','Device','ScreenHeight','ScreenWidth','refferal','statTimeStamp'],axis=1)
 main2.line_chart(webTraffic)
+
+
+
+from sklearn.model_selection import train_test_split
+TFData = pd.read_csv("data/training1.csv")
+y=TFData['label']
+X = pd.get_dummies(TFData.drop(['entryID','label'], axis=1))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.6)
+
+
+from tensorflow.keras.models import Sequential,load_model
+from tensorflow.keras.layers import Dense
+from sklearn.metrics import accuracy_score
+
+#TFmodel= tf.keras.models.load_model("Hellla Experimental.keras")
+
+TFmodel= Sequential()
+TFmodel.add(Dense(units=100,activation='sigmoid',input_dim=len(X_train.columns)))
+TFmodel.add(tf.keras.layers.Dropout(0.2, input_shape=(10,)))
+TFmodel.add(Dense(units=10,activation='sigmoid'))
+TFmodel.add(Dense(units=1,activation='relu'))
+
+TFmodel.compile(loss=tf.keras.losses.MeanAbsoluteError(),optimizer='adam',metrics=[tf.keras.metrics.Accuracy()])
+
+
+#main3.write(y_test)
+#main3.write(y_prediction)
+
+dataFun = []
+dataFun2=[]
+lol = main3.line_chart(dataFun)
+lol2 = main3.line_chart(dataFun2)
+
+#TFmodel= tf.keras.models.load_model("Hellla Experimental.keras")
+for x in range(15):
+    TFmodel.fit(X_train, y_train, epochs=200)
+    y_prediction = TFmodel.predict(X_train)
+    y_prediction=np.round(y_prediction,0)
+    dataFun.append(accuracy_score(y_train,y_prediction))
+
+    y_prediction = TFmodel.predict(X_test)
+    y_prediction=np.round(y_prediction,0)
+    dataFun2.append(accuracy_score(y_test,y_prediction))
+    lol.add_rows(dataFun)
+    lol2.add_rows(dataFun2)
+
+TFmodel.save('Hellla Experimental.keras')
